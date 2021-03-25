77f9891e682b9895b83cb962cceb90bb6d127601
devstart20 <ninjadev2020@gmail.com>
2021-03-25 05:48:06 +0200

    Update pixls.routes.js

--- app/routes/pixls.routes.js
+++ app/routes/pixls.routes.js
@@ -3,6 +3,9 @@ module.exports = app => {
     var router = require('express').Router();
 
     //Retrieve all pixls
+    router.get('/', function(req, res){
+        res.send("hello world");
+    } )
     router.post("/", pixls.findAll);
     router.post("/filter", pixls.findFilter );
     app.use('/api/pixls', router);
